2c14d2baa1ad2a993fec065a261fcaf109c223a8
alexlogy <alex@alexlogy.com>
2020-10-09 00:17:26 +0800

    add channels management and added decorator for role_check

--- app.py
+++ app.py
@@ -23,6 +23,16 @@ def auth_required(f):
             return f(*args, **kwargs)
     return decorated_function
 
+def role_check(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session['role'] == 'Student':
+            message = "You're not authorized to perform this function!"
+            alert_type = 'danger'
+            return redirect(url_for('dashboard', next=request.url, message=message, alert_type=alert_type))
+        else:
+            return f(*args, **kwargs)
+    return decorated_function
 
 def create_demoadmin():
     role = 'staff'
@@ -66,6 +76,7 @@ def create_demostudent():
     }
     results = mongo.db.users.insert_one(user)
     print ('Created User demostudent (id: %s)' % results.inserted_id)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -112,20 +123,221 @@ def logout():
     '''
     Unset Sessions
     '''
+    session.pop('id')
     session.pop('role')
     session.pop('fullname')
     session.pop('username')
 
     return redirect(url_for('login'))
 
+@app.route('/changepassword', methods=['GET', 'POST'])
+@auth_required
+def change_password():
+        message = ''
+        alert_type = ''
+        user_details = mongo.db.users.find_one({"_id": ObjectId(session['id'])})
+
+        if request.method == 'POST':
+            password = request.form.get('password')
+
+            if password:
+                password = hashlib.md5(password.encode('utf-8')).hexdigest()
+            else:
+                message = 'Nothing was changed!'
+                alert_type = 'info'
+                return redirect(url_for('dashboard', message=message, alert_type=alert_type))
+
+            now = datetime.now()
+            modified_datetime = now.strftime("%d/%m/%Y %H:%M:%S")
+            modified_by = session['username']
+
+            user_id = {
+                "_id": ObjectId(session['id'])
+            }
+            user = {
+                "$set": {
+                    "password": password,
+                    "modified_by": modified_by,
+                    "modified_datetime": modified_datetime
+                }
+            }
+            results = mongo.db.users.find_one_and_update(user_id, user)
+            message = 'Successfully change password for user (id: %s)' % results['_id']
+            alert_type = 'success'
+            return redirect(url_for('dashboard', message=message, alert_type=alert_type))
+        return render_template('change_password.html', message=message, alert_type=alert_type, user=user_details)
+
+
 @app.route('/')
 @auth_required
-def dashboard():
-    return render_template('dashboard.html')
+def dashboard(message='', alert_type=''):
+    if request.args.get('message') and request.args.get('alert_type'):
+        message = request.args.get('message')
+        alert_type = request.args.get('alert_type')
 
+    return render_template('dashboard.html', message=message, alert_type=alert_type)
+
+
+'''
+Channels
+'''
+@app.route('/channels/list')
+@auth_required
+def list_channels(message='', alert_type=''):
+        if request.args.get('message') and request.args.get('alert_type'):
+            message = request.args.get('message')
+            alert_type = request.args.get('alert_type')
+
+        channel_list = mongo.db.channels.find()
+        return render_template('list_channels.html', message=message, alert_type=alert_type, channels_list=channel_list)
+
+
+@app.route('/channels/create', methods=['GET', 'POST'])
+@auth_required
+@role_check
+def create_channel(message='', alert_type=''):
+        if request.args.get('message') and request.args.get('alert_type'):
+            message = request.args.get('message')
+            alert_type = request.args.get('alert_type')
+
+        if request.method == 'POST':
+            channelname = request.form.get('channelname')
+            channeldate = request.form.get('channeldate')
+            starttime = request.form.get('starttime')
+            endtime = request.form.get('endtime')
+            capacity = int(request.form.get('capacity'))
+
+            if channelname and channeldate and starttime and endtime and capacity:
+                try:
+                    date_validation = datetime.strptime(channeldate, "%d/%m/%Y")
+                    starttime_validation = datetime.strptime(starttime, "%H%M")
+                    endtime_validation = datetime.strptime(endtime, "%H%M")
+                except ValueError:
+                    message = 'Invalid Date/Time Format! (dd/mm/yy, HHHH)'
+                    alert_type = 'danger'
+                    return redirect(url_for('create_channel', message=message, alert_type=alert_type))
+
+                if (endtime > starttime):
+                    if (isinstance(capacity, int) and (capacity > 0)):
+                        now = datetime.now()
+                        created_datetime = now.strftime("%d/%m/%Y %H:%M:%S")
+                        created_by = session['username']
+
+                        channel = {
+                            "channelname": channelname,
+                            "channeldate": channeldate,
+                            "starttime": starttime,
+                            "endtime": endtime,
+                            "capacity": capacity,
+                            "created_by": created_by,
+                            "created_datetime": created_datetime,
+                            "modified_datetime": created_datetime
+                        }
+                        results = mongo.db.channels.insert_one(channel)
+                        message = 'Successfully created channel (id: %s)' % results.inserted_id
+                        alert_type = 'success'
+                        return redirect(url_for('list_channels', message=message, alert_type=alert_type))
+                    else:
+                        message = 'Invalid Channel Capacity!'
+                        alert_type = 'danger'
+                        return redirect(url_for('create_channel', message=message, alert_type=alert_type))
+                else:
+                    message = 'Invalid Start and End Time'
+                    alert_type = 'danger'
+                    return redirect(url_for('create_channel', message=message, alert_type=alert_type))
+            else:
+                message = "Please fill in all the fields!"
+                alert_type = 'danger'
+        return render_template('create_channel.html', message=message, alert_type=alert_type)
+
+@app.route('/channels/edit/<channelid>', methods=['GET', 'POST'])
+@auth_required
+@role_check
+def edit_channel(channelid, message='', alert_type=''):
+        if request.args.get('message') and request.args.get('alert_type'):
+            message = request.args.get('message')
+            alert_type = request.args.get('alert_type')
+
+        channel_details = mongo.db.channels.find_one({"_id": ObjectId(channelid)})
+
+        if request.method == 'POST':
+            channelname = request.form.get('channelname')
+            channeldate = request.form.get('channeldate')
+            starttime = request.form.get('starttime')
+            endtime = request.form.get('endtime')
+            capacity = int(request.form.get('capacity'))
+
+            if channelname and channeldate and starttime and endtime and capacity:
+                try:
+                    date_validation = datetime.strptime(channeldate, "%d/%m/%Y")
+                    starttime_validation = datetime.strptime(starttime, "%H%M")
+                    endtime_validation = datetime.strptime(endtime, "%H%M")
+                except ValueError:
+                    message = 'Invalid Date/Time Format! (dd/mm/yy, HHHH)'
+                    alert_type = 'danger'
+                    return redirect(url_for('create_channel', message=message, alert_type=alert_type))
+
+                if (endtime > starttime):
+                    if (isinstance(capacity, int) and (capacity > 0)):
+                        now = datetime.now()
+                        modified_datetime = now.strftime("%d/%m/%Y %H:%M:%S")
+                        modified_by = session['username']
+
+                        channel = {
+                            "channelname": channelname,
+                            "channeldate": channeldate,
+                            "starttime": starttime,
+                            "endtime": endtime,
+                            "capacity": capacity,
+                            "modified_by": modified_by,
+                            "modified_datetime": modified_datetime
+                        }
+                        results = mongo.db.channels.insert_one(channel)
+                        message = 'Successfully edited channel (id: %s)' % results.inserted_id
+                        alert_type = 'success'
+                        return redirect(url_for('list_channels', message=message, alert_type=alert_type))
+                    else:
+                        message = 'Invalid Channel Capacity!'
+                        alert_type = 'danger'
+                        return redirect(url_for('edit_channel', message=message, alert_type=alert_type))
+                else:
+                    message = 'Invalid Start and End Time'
+                    alert_type = 'danger'
+                    return redirect(url_for('edit_channel', message=message, alert_type=alert_type))
+            else:
+                message = "Please fill in all the fields!"
+                alert_type = 'danger'
+        return render_template('edit_channel.html', message=message, alert_type=alert_type, channel=channel_details)
+
+
+@app.route('/channels/delete/<channelid>', methods=['GET', 'POST'])
+@auth_required
+@role_check
+def delete_channel(channelid):
+        message = ''
+        alert_type = ''
+        channel_details = mongo.db.channels.find_one({"_id": ObjectId(channelid)})
+
+        if request.method == 'POST':
+            channel_id = {
+                "_id": ObjectId(channelid)
+            }
+
+            results = mongo.db.channels.find_one_and_delete(channel_id)
+            message = 'Successfully deleted channel (id: %s)' % results['_id']
+            alert_type = 'success'
+            return redirect(url_for('list_channels', message=message, alert_type=alert_type))
+
+        return render_template('delete_channel.html', message=message, alert_type=alert_type, channel=channel_details)
+
+
+'''
+Users
+'''
 
 @app.route('/users/list')
 @auth_required
+@role_check
 def list_users(message='', alert_type=''):
         if request.args.get('message') and request.args.get('alert_type'):
             message = request.args.get('message')
@@ -136,6 +348,7 @@ def list_users(message='', alert_type=''):
 
 @app.route('/users/create', methods=['GET', 'POST'])
 @auth_required
+@role_check
 def create_user():
         message = ''
         alert_type = ''
@@ -171,6 +384,7 @@ def create_user():
 
 @app.route('/users/edit/<userid>', methods=['GET', 'POST'])
 @auth_required
+@role_check
 def edit_user(userid):
         message = ''
         alert_type = ''
@@ -216,6 +430,7 @@ def edit_user(userid):
 
 @app.route('/users/delete/<userid>', methods=['GET', 'POST'])
 @auth_required
+@role_check
 def delete_user(userid):
         message = ''
         alert_type = ''
---
+++ static/js/main.js
@@ -0,0 +1,47 @@
+//datepicker
+
+$( function() {
+    $( "#channeldate" ).datepicker({
+      dateFormat: "dd/mm/yy"
+    });
+
+    $('#starttime').on("input change", function() {
+        //make sure user can only input 4 numbers
+        if ($(this).val().length > 4) {
+            this.value = $(this).val().slice(0,4);
+        }
+
+        var starttime = $(this).val()
+        var starttime_validator = moment(starttime, "HHHH", true).isValid();
+
+        if (!starttime_validator) {
+            $('#starttimeHelp').text('Please enter time in HHHH format.');
+            $('#channel-submit-btn').prop('disabled', true);
+        } else {
+            $('#channel-submit-btn').prop('disabled', false);
+        }
+    });
+
+    $('#endtime').on("input change", function() {
+        //make sure user can only input 4 numbers
+        if ($(this).val().length > 4) {
+            this.value = $(this).val().slice(0,4);
+        }
+
+        var starttime = $('#starttime').val()
+        var endtime = $(this).val()
+        var endtime_validator = moment(endtime, "HHHH", true).isValid();
+        var endtimeafter_validator = moment(endtime).isAfter(starttime);
+
+        if (!endtime_validator) {
+            $('#channel-submit-btn').prop('disabled', true);
+        } else {
+            if (!endtimeafter_validator) {
+                $('#channel-submit-btn').prop('disabled', true);
+            } else {
+                $('#channel-submit-btn').prop('disabled', false);
+            }
+        }
+
+    })
+} );
